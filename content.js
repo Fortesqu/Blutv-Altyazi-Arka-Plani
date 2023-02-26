75cff32fbc506a3f0e51c4b33661fb791c500eb5
Fortesqu <emre_viper@hotmail.com>
2023-02-26 18:49:32 +0300

    Add files via upload

---
+++ content.js
@@ -0,0 +1,6 @@
+const css = '.season-episode-selector{padding:0!important}.theoplayer-webvtt-region-default span{background:rgb(0 0 0 / 80%)!important}';
+const style = document.createElement('style');
+style.setAttribute('type', 'text/css');
+style.setAttribute('data-styled-jsx', '');
+style.appendChild(document.createTextNode(css));
+document.body.appendChild(style);
---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(tabs[0].id, {file: "content.js"});
+    });
+  });
+});
